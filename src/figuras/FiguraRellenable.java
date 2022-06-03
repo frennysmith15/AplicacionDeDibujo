2aa0b832f06e7b0852fcb55142743a357edbe8f4
frennysmith15@hotmail.com <frennysmith15@hotmail.com>
2022-06-03 17:58:34 -0400

    FiguraRellenable implementado.

---
+++ src/figuras/FiguraRellenable.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package figuras;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+/**
+ *
+ * @author frenn
+ */
+public class FiguraRellenable extends Figura{
+    
+    protected Color colorDeFondo;
+    protected Color colorDeContorno;
+    protected Boolean relleno;
+
+    public FiguraRellenable(Color colorDeFondo, Color colorDeContorno, Boolean relleno){
+        this.colorDeContorno = colorDeContorno;
+        this.colorDeFondo = colorDeFondo;
+        this.relleno = relleno;
+    }
+    
+    
+    
+    @Override
+    public void dibujar(Graphics g) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void actualizar(Point puntoActual) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
